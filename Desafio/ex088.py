15fbe47cbf535a5ccc5f086cad458ac96425538a
Nathan Gabriel Muniz da Silva <natangabriel.palmeiras@gmail.com>
2020-08-10 17:05:34 -0300

    Creat ex088

---
+++ Desafio/ex088.py
@@ -0,0 +1,19 @@
+from random import randint
+from time import sleep
+print(20 * "-=")
+print(10 * " " +  "JOGA NA MEGA SENA")
+print(20 * "-=")
+n = int(input("Quantos você quer que eu sortei ? "))
+print(3 * "-=" + f"SORTEANDO {n} JOGOS " + 3 * "-=" )
+
+lista = listaPrin =  list()
+for jog in range(1, n + 1):
+    listaPrin.append(lista[:])
+    lista.clear()
+    for c in range(0, 6):
+        pc = randint(1, 60)
+        if pc not in lista:
+            lista.append(pc)
+    print(f"Jogo {jog}: {lista}")
+    sleep(0.8)
+print(20 * "-=" + " < BOA SORTE! >" + 20 * "-=")
