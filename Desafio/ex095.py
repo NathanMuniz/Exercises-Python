373e57536e66e1e053aa731d495b9868926c0e91
Nathan Gabriel <natangabriel.palmeiras@gmail.com>
2020-09-11 15:37:35 -0300

    Adicionei ex095

--- Desafio/ex095.py
+++ Desafio/ex095.py
@@ -5,6 +5,7 @@ goals = list()
 total = 0
 # Ler os dados dos jogadores 
 while run: 
+    jogador.clear()
     # Ler nome   
     jogador["nome"] = str(input("Nome do jogador: "))
     # Ler Total de paridas
@@ -20,14 +21,45 @@ while run:
     jogador["total"] = total
     total = 0   
     jogadores.append(jogador.copy())
-    jogador.clear()
 
     # Quer continuar 
-    resp = str(input("Quer contrinuar [S/N]: ")).upper()
+    while True:
+        resp = str(input("Quer contrinuar [S/N]: ")).upper()[0]  
+        if resp in "SN":
+            break
+        print("[ERRO] respnda apenas S ou N")
     if resp == "N":
         run = False
 
+
 # Mostra Dados
-print(20 * "-=")
-print("cod nome      goals       total")
-print(20 * "-")
+
+# Fazer cabeçalho
+print(30 * "-=")
+print('cod ', end="")
+for i in jogador.keys():
+    print(f"{i:<15}", end="")
+print()
+
+# Mostrar dados
+print(30 * "-")
+for k, v in enumerate(jogadores):
+    print(f"{k:>3}", end="")
+    for d in v.values():
+        print(f" {str(d):<15}", end="")
+    print()
+print("-" * 40)
+
+# Mostrar Dados de jogadores Específicos
+while True:
+    resp = int(input("Mostar dados de qual jogador [999 para parar]: "))
+    if resp == 999:
+        break
+    elif resp >= len(jogadores):
+        print(f"[ERRO] não existe jogador com o código {resp}")
+    else:
+        print(f"-- LEVANTAMENTO DO JOGADOR {jogadores[resp]['nome']}")
+        for k, v in enumerate(jogadores[resp]['goal']):
+            print(f"    No jogo {k + 1} fez {v}")
+    print(40 * "-")
+print("<< VOLTE SEMPRE >>") 
