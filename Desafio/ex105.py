f97593edd6ddd35e05b411564bf9735abdc98245
Nathan Gabriel <natangabriel.palmeiras@gmail.com>
2020-09-26 15:46:25 -0300

    Fix ex 105

--- Desafio/ex105.py
+++ Desafio/ex105.py
@@ -1,12 +1,31 @@
 def notas(*n, sit=False):
+    """
+    notas(*n, sir=False)
+
+        --> Funcao para analisar notas de Alunos.
+        :param n: Notas dos alunos (aceita varias).
+        :param sit: Valor opocional, indica se deve ou não adicionar a situação.
+        :param return: Dicionario com várias informações sobre a turma.
+    """
     notas = dict()
-    for v in n:
-        notas["total"] = len(n)
-        mai = v
-        if v > mai:
-            notas["maioe"] = mai
+    # Adicionar valores no dicionario
+    notas = dict()
+    notas["total"] = len(n)
+    notas["mai"] = max(n)
+    notas["men"] = min(n)
+    notas["media"] = sum(n) / len(n)
+    # Identificar situação 
+    if 8 > notas["media"] >= 5:
+        situacao = "RAZOÁVEL"
+    elif notas["media"] < 5:
+        situacao = "RUIM"
+    else:
+        situacao = "BOA"
+    if  sit:
+        notas["situação"] = situacao
+    # Retornar
     return notas
 
 # Main Program
-resp = notas(5.5, 2.5, 1.5)  
+resp = notas(10, 8, sit=True)  
 print(resp)
